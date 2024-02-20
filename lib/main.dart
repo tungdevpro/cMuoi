44da1d49a22e9cab4c524d61696e4223faf4364c
tungdevpro <tungdo21899@gmail.com>
2024-02-20 22:40:01 +0700

    bugfix: change main func

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,16 @@ import 'package:data/data.dart';
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:gen_artify/di/di.dart';
+import 'package:gen_artify/gen_arrtify.dart';
 
-void main() {
+void main() async {
+  await _prepare();
+  runApp(const GenArtify());
+}
+
+Future<void> _prepare() async {
+  WidgetsFlutterBinding.ensureInitialized();
   DataLayer.init();
   DomainLayer.init();
   configureDependencies();
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(),
-    );
-  }
 }
