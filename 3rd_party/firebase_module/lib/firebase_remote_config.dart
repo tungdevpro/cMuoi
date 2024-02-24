730363e9501e195fb789cce53da52dfd3085ec04
tungdevpro <tungdo21899@gmail.com>
2024-02-24 08:39:35 +0700

    feat: add firebase module

--- 3rd_party/firebase_module/lib/firebase_remote_config.dart
+++ 3rd_party/firebase_module/lib/firebase_remote_config.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 class CustomFirebaseRemoteConfig {
+  late FirebaseRemoteConfig remoteConfig;
   static final CustomFirebaseRemoteConfig _inst = CustomFirebaseRemoteConfig._internal();
 
-  CustomFirebaseRemoteConfig._internal();
+  CustomFirebaseRemoteConfig._internal() {
+    remoteConfig = FirebaseRemoteConfig.instance;
+  }
 
   factory CustomFirebaseRemoteConfig() {
     return _inst;
   }
-
-  final remoteConfig = FirebaseRemoteConfig.instance;
 
   void fetch() async {
     final res = await remoteConfig.fetch();
