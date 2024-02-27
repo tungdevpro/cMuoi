f2fb9f17b0d39c3bbea1fe6e61b439ef4e856802
tungdevpro <tungdo21899@gmail.com>
2024-02-27 10:31:03 +0700

    feat: change location image of social meida

--- lib/common/constants/icon_resource.dart
+++ lib/common/constants/icon_resource.dart
@@ -8,18 +8,18 @@ abstract class IconResource {
   static const String errorMessage = 'assets/icons/error_message.svg';
   static const String logoText = 'assets/icons/Logo_text.svg';
   static const String maskIntroBackground = 'assets/icons/Mask-Intro-Background.svg';
-  static const String navCamera = 'assets/icons/Nav-Camera.svg';
   static const String navCameraLight = 'assets/icons/Nav-Camera-Light.svg';
   static const String navCameraSelected = 'assets/icons/Nav-Camera-Selected.svg';
-  static const String navHome = 'assets/icons/Nav-Home.svg';
+  static const String navCamera = 'assets/icons/Nav-Camera.svg';
   static const String navHomeLight = 'assets/icons/Nav-Home-Light.svg';
   static const String navHomeSelected = 'assets/icons/Nav-Home-Selected.svg';
-  static const String navMessage = 'assets/icons/Nav-Message.svg';
+  static const String navHome = 'assets/icons/Nav-Home.svg';
   static const String navMessageLight = 'assets/icons/Nav-Message-Light.svg';
   static const String navMessageSelected = 'assets/icons/Nav-Message-Selected.svg';
-  static const String navProfile = 'assets/icons/Nav-Profile.svg';
+  static const String navMessage = 'assets/icons/Nav-Message.svg';
   static const String navProfileLight = 'assets/icons/Nav-Profile-Light.svg';
   static const String navProfileSelected = 'assets/icons/Nav-Profile-Selected.svg';
+  static const String navProfile = 'assets/icons/Nav-Profile.svg';
   static const String visibilityOff = 'assets/icons/visibility-off.svg';
   static const String visibilityOn = 'assets/icons/visibility-on.svg';
 
--- lib/common/constants/image_resource.dart
+++ lib/common/constants/image_resource.dart
@@ -1,5 +1,11 @@
 abstract class ImageResource {
   const ImageResource._();
 
+  static const String facebook = 'assets/images/Facebook.png';
+  static const String google = 'assets/images/Google.png';
   static const String logo = 'assets/images/logo.png';
+  static const String twitter = 'assets/images/Twitter.png';
+  static const String wechat = 'assets/images/Wechat.png';
+  static const String whatsapp = 'assets/images/Whatsapp.png';
+
 }
