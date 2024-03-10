795d3bd91a715b9f95fa0db2d2fabde5745842b2
tungdevpro <tungdo21899@gmail.com>
2024-03-10 09:30:43 +0700

    feat(dio): add auth interceptor to data layer

--- data/lib/datasource/remote/api_client.dart
+++ data/lib/datasource/remote/api_client.dart
@@ -1,11 +1,14 @@
 import 'package:data/datasource/remote/service/sign_up_service.dart';
+import 'package:data/utils/auth_interceptor.dart';
 import 'package:injectable/injectable.dart';
 import '../../network/dio_client.dart';
 
 @singleton
 class ApiClient {
   final DioClient dio;
-  ApiClient(this.dio) {}
+  ApiClient(this.dio) {
+    dio.init(interceptors: [AuthInterceptor()]);
+  }
 
   SignUpService get signup => SignUpService(dio.build());
 }
---
+++ data/lib/utils/auth_interceptor.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+
+class AuthInterceptor extends InterceptorsWrapper {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    super.onError(err, handler);
+  }
+}
